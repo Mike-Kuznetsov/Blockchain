b8f172ffa289c12706fe9298b0843c0b1773beaa
Mikhail Kuznetsov <61430641+Mike-Kuznetsov@users.noreply.github.com>
2022-06-13 13:17:11 +0300

    Update and rename blockchain_v3.cpp to blockchain_v2.cpp

--- blockchain_v2.cpp
+++ blockchain_v2.cpp
@@ -1,3 +1,23 @@
+/* 
+*   Blockchain for Mautoz Tech Youtube Video.
+*   Version 2. Added proof of work and mining. Still no users and transactions.
+*   This program is simpliest implementation of blockchain technology.
+*
+*   Mautoz Tech - https://www.youtube.com/c/MautozTech
+*   Заметки ESPшника - https://www.youtube.com/channel/UCQAbEIaWFdARXKqcufV6y_g
+*
+*    This program is free software: you can redistribute it and/or modify 
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*    This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*    You should have received a copy of the GNU General Public License
+*  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include <iostream>
 #include "sha256.h"
 #include <thread>
