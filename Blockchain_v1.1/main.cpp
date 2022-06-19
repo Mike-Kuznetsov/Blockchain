d55cd47743acf707b055084219e949706425525f
Mikhail Kuznetsov <61430641+Mike-Kuznetsov@users.noreply.github.com>
2022-06-19 14:07:13 +0300

    Class Blockchain divided into classes User & Miner

--- Blockchain_v1.1/main.cpp
+++ Blockchain_v1.1/main.cpp
@@ -1,32 +1,34 @@
+/* 
+*   Blockchain for Mautoz Tech Youtube Video.
+*   Version 1.1. Now classes are in separate files. Class Blockchain has been divided into classes User and Miner. 
+*   When miner finds block it sends it to user which collects correct blocks into blockchain.
+*   This program is simpliest implementation of blockchain technology.
+*
+*   Mautoz Tech - https://www.youtube.com/c/MautozTech
+*   Заметки ESPшника - https://www.youtube.com/channel/UCQAbEIaWFdARXKqcufV6y_g
+*
+*    This program is free software: you can redistribute it and/or modify 
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*    This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*    You should have received a copy of the GNU General Public License
+*  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include <iostream>
 #include "miner.cpp"
 
 using namespace std;
-
-/*class User {
-    public:
-        int k=0;
-        void setUsers(this setUser1, User setUser2, User setUser3){
-            user=setUser1;
-            user=setUser1;
-            user=setUser1;
-            k=1;
-        }
-
-};*/
-
-/*class Transaction {
-    public:
-        int number, value;
-        string sender, receiver, sign;
-};*/
 
 int main(int argc, char *argv[])
 {
     User user1;
     Miner miner1;
     miner1.start(&user1);
-    //user1.setMiners(miner1);
     string input;
     while(true){
          cin >> input;
@@ -46,10 +48,5 @@ int main(int argc, char *argv[])
             miner1.addData(input);
          }
     }
-    //myCoin.block[1].data="KEK";
-    //myCoin.block[1].thisHash=myCoin.block[1].getHash();
-    //myCoin.block[2].prevHash=myCoin.block[1].thisHash;
-    //myCoin.block[2].thisHash=myCoin.block[2].getHash();
-    //cout << "kek";
     return 0;
 }
