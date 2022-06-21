33ac6ecf111e71b2e5b42de4ad1f03188b24ec88
Mikhail Kuznetsov <61430641+Mike-Kuznetsov@users.noreply.github.com>
2022-06-21 22:22:13 +0300

    Update user.h

--- Blockchain_v1.4/user.h
+++ Blockchain_v1.4/user.h
@@ -3,17 +3,13 @@
 
 #include <string>
 #include "block.h"
-//#include "miner.h"
 
 class User {
     public:
-        //Miner miner;
         int lastBlock;
         Block block[10000];
         User();
         void setBlock(Block receivedBlock);
-        //void setMiners(Miner miner1);
-        //void addData(std::string newData);
         bool check();
         void showChain();
         void showData();
