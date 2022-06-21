876879d4bcc0a2dec37df899215a17deefa9be5a
Mikhail Kuznetsov <61430641+Mike-Kuznetsov@users.noreply.github.com>
2022-06-21 16:33:40 +0300

    Added header files

--- Blockchain_v1.3/main.cpp
+++ Blockchain_v1.3/main.cpp
@@ -1,23 +1,34 @@
+/* 
+*   Blockchain for Mautoz Tech Youtube Video.
+*   Version 1.3. Added header files
+*   In this project i write simpliest cryptocurrency for my youtube channel.
+*
+*   Mautoz Tech - https://www.youtube.com/c/MautozTech
+*   Заметки ESPшника - https://www.youtube.com/channel/UCQAbEIaWFdARXKqcufV6y_g
+*
+*    This program is free software: you can redistribute it and/or modify 
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*    This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*    You should have received a copy of the GNU General Public License
+*  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include <iostream>
 #include <regex>
 #include "miner.h"
 #include "user.h"
 using namespace std;
 
-/*class Transaction {
-    public:
-        int number, value;
-        string sender, receiver, sign;
-};*/
-
-
 int main(int argc, char *argv[])
 {
     User *user = new User[4];
-    Miner *miner= new Miner[4];
-    miner[0].start(user); // miner
-    //miner[1].start(user, miner);
-    //user1.setMiners(miner1);
+    Miner *miner= new Miner[1];
+    miner[0].start(user);
     string input;
     cmatch result;
     int ID;
@@ -74,9 +85,6 @@ int main(int argc, char *argv[])
          }
          else{
             miner[0].addData(input);
-            //miner[1].addData(input);
-            //miner[2].addData(input);
-            //miner[3].addData(input);
          }
     }
     return 0;
