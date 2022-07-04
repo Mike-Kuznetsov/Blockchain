2bc7b8c47c0db8a428fc4ce05a5489e3435d75b7
Mikhail Kuznetsov <61430641+Mike-Kuznetsov@users.noreply.github.com>
2022-07-04 23:23:00 +0300

    Added crypto transfers for every user

---
+++ Blockchain_v2.3/block.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "sha256.h"
+#include "block.h"
+#include "transaction.h"
+#include "signature.h"
+
+using namespace std;
+
+void Block::setContent(int setNumber, string setPrevHash, int setBalances[9], int setTransCount[9], Signature *setSignature){
+    prevHash=setPrevHash;
+    number=setNumber;
+    nonce=0;
+    hash="";
+    i=0;
+    signature=setSignature;
+    for (int i=0;i<9;i++){
+        balances[i]=setBalances[i];
+        transactionCount[i]=setTransCount[i];
+    }
+    //data="";
+}
+
+void Block::addTransaction(Transaction *transaction){
+    //data+=newData;
+    transactions[i]=*transaction;
+    i++;
+}
+
+string Block::hashFunc(){
+    return sha256(to_string(number) + data + prevHash + to_string(nonce));
+}
