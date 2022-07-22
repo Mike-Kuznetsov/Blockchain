18a67a1fb9a060101a8c41447f6369927ba5a739
Mikhail Kuznetsov <61430641+Mike-Kuznetsov@users.noreply.github.com>
2022-07-22 19:48:25 +0300

    Interface with RegEx and more than 1 user

--- Blockchain_v1.2/main.cpp
+++ Blockchain_v1.2/main.cpp
@@ -1,3 +1,23 @@
+/* 
+*   Blockchain for Mautoz Tech Youtube Video.
+*   Version 1.2. Added interface with RegEx and more than 1 user & more than 1 blockchain.
+*   This program is simpliest implementation of blockchain technology.
+*
+*   Mautoz Tech - https://www.youtube.com/c/MautozTech
+*   Заметки ESPшника - https://www.youtube.com/channel/UCQAbEIaWFdARXKqcufV6y_g
+*
+*    This program is free software: you can redistribute it and/or modify 
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*    This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*    You should have received a copy of the GNU General Public License
+*  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include <iostream>
 #include "user.h"
 #include "miner.h"
@@ -68,23 +88,5 @@ int main()
             //miner[3].addData(input);
          }
     }
-    /*while(true){
-         cin >> input;
-         if (input=="showData"){
-            user[0].showData();
-         }
-         else if (input=="showChain"){
-            user[0].showChain();
-         }
-         else if (input=="check"){
-            if (user[0].check())
-                cout << "Blockchain is valid" << endl;
-            else
-                cout << "Blockchain is corrupted" << endl;
-            }
-         else{
-            miner.addData(input);
-         }
-    }*/
     return 0;
 }
