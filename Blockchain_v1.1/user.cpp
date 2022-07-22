47e4bebc9f299ed5c6830ef62af7ae3c0b0bfa4a
Mikhail Kuznetsov <61430641+Mike-Kuznetsov@users.noreply.github.com>
2022-07-22 15:05:56 +0300

    Update user.cpp

--- Blockchain_v1.1/user.cpp
+++ Blockchain_v1.1/user.cpp
@@ -1,56 +1,49 @@
 #include <iostream>
-#include "block.cpp"
-#include <thread>
+#include "block.h"
+#include "user.h"
 
 using namespace std;
 
-class User {
-    public:
-        int lastBlock;
-        Block block[10000];
-
-        User(){
-            block[0].setContent(0,"");
-            lastBlock=0;
+void User::start(int setDifficulty){
+    block[0].createBlock(0,"0");
+    block[0].hash=block[0].hashFunc();
+    lastBlock=0;
+    difficulty=setDifficulty;
+    zeros="000000000000000000000000000000";
+    zeros=zeros.substr(0, difficulty);
+}
+void User::setBlock(Block receivedBlock){
+    //cout << zeros << endl;
+    //cout << receivedBlock.hashFunc().substr(0, difficulty) << endl;
+    //if (receivedBlock.hashFunc().substr(0, difficulty)==zeros) cout << "true" << endl;
+    if ((receivedBlock.hashFunc().substr(0, difficulty)==zeros) && (receivedBlock.number==block[lastBlock].number+1) && (receivedBlock.prevHash==block[lastBlock].hash)){
+        lastBlock++;
+        block[lastBlock]=receivedBlock;
+    }
+    //lastBlock++;
+    //block[lastBlock].createBlock(lastBlock, block[lastBlock-1].hash);
+}
+bool User::check(){
+    for (int i=1; i<=lastBlock; i++){
+        if (block[i].hash != block[i].hashFunc()){
+            return false;
         }
-        void setBlock(Block receivedBlock){
-            if ((receivedBlock.hashFunc()==receivedBlock.hash) && (receivedBlock.number==block[lastBlock].number+1) && (receivedBlock.prevHash==block[lastBlock].hash)){
-                lastBlock++;
-                block[lastBlock]=receivedBlock;
-            }
-            // ELSE Сравнение своего блокчейна с другими блокчейнами и бан майнера приславшего блок
-
+        else if (block[i].prevHash != block[i-1].hashFunc()){
+            return false;
         }
-        /*void User::setMiners(Miner miner1){
-            miner=miner1;
-        }*/
-        /*void User::addData(string newData){
-            block[lastBlock].addData(newData);
-        }*/
-        bool check(){
-            for (int i=1; i<lastBlock; i++){
-                if (block[i].hash != block[i].hashFunc()){
-                    return false;
-                }
-                else if (block[i].prevHash != block[i-1].hash){
-                    return false;
-                }
-            }
-            return true;
-        }
-        void showChain(){
-            for (int i; i<=lastBlock; i++){
-                cout << "Block: " << i << endl;
-                cout <<  block[i].prevHash << endl;
-                cout <<  block[i].hash << endl;
-            }
-            cout << "Chain" << endl;
-        }
-        void showData(){
-            for (int i; i<=lastBlock; i++){
-                cout << "Block: " << i << endl;
-                cout <<  block[i].data << endl;
-            }
-        }
-
-};
+    }
+    return true;
+}
+void User::showChain(){
+    for (int i; i<=lastBlock; i++){
+        cout << "Block: " << i << endl;
+        cout <<  block[i].prevHash << endl;
+        cout <<  block[i].hash << endl;
+    }
+}
+void User::showData(){
+    for (int i; i<=lastBlock; i++){
+        cout << "Block: " << i << endl;
+        cout <<  block[i].data << endl;
+    }
+}
