a25f579e85f32198884bb9e4c2c237dad2163854
Mikhail Kuznetsov <61430641+Mike-Kuznetsov@users.noreply.github.com>
2022-07-23 18:00:02 +0300

    User can ask other users for blocks

---
+++ Blockchain_v1.3/user.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include "block.h"
+#include "user.h"
+
+using namespace std;
+
+void User::start(int setDifficulty, int ID, User *setUser){
+    myID=ID;
+    user=setUser;
+    block[0].createBlock(0,"0");
+    block[0].hash=block[0].hashFunc();
+    lastBlock=0;
+    difficulty=setDifficulty;
+    zeros="000000000000000000000000000000";
+    zeros=zeros.substr(0, difficulty);
+}
+
+bool User::checkBlock(Block *receivedBlock){
+    if (((*receivedBlock).prevHash==block[lastBlock].hash)
+    && ((*receivedBlock).hashFunc().substr(0, difficulty)==zeros)
+    && ((*receivedBlock).hashFunc()==(*receivedBlock).hash)){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+void User::setBlock(Block receivedBlock){
+    if (receivedBlock.number==block[lastBlock].number+1){
+        if (checkBlock(&receivedBlock)){
+            lastBlock++;
+            block[lastBlock]=receivedBlock;
+        }
+    }
+    else{
+        int maxBlock=lastBlock;
+        int userID;
+        for (int i=0; i<4; i++){
+            if (user[i].lastBlock>maxBlock){
+                maxBlock=user[i].lastBlock;
+                userID=i;
+            }
+        }
+        if (maxBlock>lastBlock){
+            bool noErrors=true;
+            int equalBlock=0;
+            for (int i=lastBlock; i>0; i--){
+                if (user[userID].block[i].hashFunc()==block[i].hash){
+                    equalBlock=i;
+                    break;
+                }
+            }
+            int lastBlock2;
+            for (int i=equalBlock+1; i<=maxBlock; i++){
+                if (checkBlock(&user[userID].block[i])){
+                    lastBlock2=i;
+                }
+                else{
+                    noErrors=false;
+                    break;
+                }
+            }
+            if (lastBlock2>lastBlock){
+                for (int i=equalBlock+1; i<=lastBlock2; i++){
+                    lastBlock++;
+                    block[lastBlock]=user[userID].block[i];
+                }
+            }
+            if (checkBlock(&receivedBlock) && (receivedBlock.number==block[lastBlock].number+1)){
+                lastBlock++;
+                block[lastBlock]=receivedBlock;
+            }
+        }
+    }
+}
+bool User::check(){
+    for (int i=1; i<=lastBlock; i++){
+        if (block[i].hash != block[i].hashFunc()){
+            return false;
+        }
+        else if (block[i].prevHash != block[i-1].hashFunc()){
+            return false;
+        }
+    }
+    return true;
+}
+void User::showChain(){
+    for (int i=0; i<=lastBlock; i++){
+        cout << "Block: " << i << endl;
+        cout <<  block[i].prevHash << endl;
+        cout <<  block[i].hash << endl;
+    }
+}
+void User::showData(){
+    for (int i=0; i<=lastBlock; i++){
+        cout << "Block: " << i << endl;
+        cout <<  block[i].data << endl;
+    }
+}
